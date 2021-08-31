59774f1071e59c8ac39052a881cca49a2989c248
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-08-31 18:00:00 -0300

    fix clients logos

--- src/components/LogoClound/index.js
+++ src/components/LogoClound/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import logoiconfios from '../../images/logoiconfios.png'
 import logoipel from '../../images/logoipel.webp'
-import logouniasselvi from '../../images/logouniasselvi.png'
+import logouniasselvi from '../../images/logouniasselvi.webp'
 import logoartico from '../../images/logoartico.png'
 import logomuller from '../../images/logomueller.png'
+import logometisa from '../../images/logometisa.webp'
 
 const clients = [
   {
@@ -23,39 +23,10 @@ const clients = [
     src: logoartico,
   },
   {
-    href: 'https://www.incofios.com.br/',
-    src: logoiconfios,
+    href: 'https://www.metisa.com.br/',
+    src: logometisa,
   },
 ]
-
-const LogoClound2 = props => {
-  return (
-    <div className="bg-white" {...props}>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <p className="text-center text-xl font-semibold uppercase text-gray-600 tracking-wider">
-          Conheça nossos clientes
-        </p>
-        <div className="mt-6 grid grid-cols-2 gap-2 md:grid-cols-3 lg:mt-8">
-          {clients.map((c, i) => (
-            <a
-              key={c.href}
-              href={c.href}
-              target="_blank"
-              className={`col-span-1 flex justify-center py-8 px-8 ${
-                i % 2 > 0
-                  ? 'bg-blue-100 hover:bg-blue-200'
-                  : 'bg-blue-600 hover:bg-blue-700'
-              }`}
-              rel="noreferrer"
-            >
-              <img className="max-h-12" src={c.src} alt="Workcation" />
-            </a>
-          ))}
-        </div>
-      </div>
-    </div>
-  )
-}
 
 const LogoClound = props => {
   return (
