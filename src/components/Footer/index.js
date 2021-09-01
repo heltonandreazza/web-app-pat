3f8de910b3bfafdac080b71e9717b42e2a5ab7b4
Helton Andreazza <heltonandreazza@Heltons-MacBook-Air.local>
2021-09-01 11:49:49 -0300

    fix footer and ctabanner example

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -36,21 +36,12 @@ const Footer = ({ navigation = DEFAULT_NAVIGATION }) => {
         >
           {navigation.map(item => (
             <div key={item.name} className="px-5 py-2">
-              {item.href?.includes('/') ? (
-                <a
-                  href={item.href}
-                  className="text-base text-gray-500 hover:text-gray-900"
-                >
-                  {item.name}
-                </a>
-              ) : (
-                <AnchorLink
-                  href={item.href}
-                  className="text-base text-gray-500 hover:text-gray-900"
-                >
-                  {item.name}
-                </AnchorLink>
-              )}
+              <a
+                href={item.href}
+                className="text-base text-gray-500 hover:text-gray-900"
+              >
+                {item.name}
+              </a>
             </div>
           ))}
         </nav>
